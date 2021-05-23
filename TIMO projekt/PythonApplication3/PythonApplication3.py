9366197025d206691933b33121e528ffcefc4aa7
Student 242050 <242050@student.pwr.edu.pl>
2021-05-23 14:29:30 +0200

    v2

--- TIMO projekt/PythonApplication3/PythonApplication3.py
+++ TIMO projekt/PythonApplication3/PythonApplication3.py
@@ -1,38 +1,16 @@
 import copy
 import math
 import random 
+from matplotlib import pyplot
 import sympy as sympy
 import numpy as np
 
 '''
-    Pure Python/Numpy implementation of the Nelder-Mead algorithm.
-    Reference: https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method
+Pure Python/Numpy implementation of the Nelder-Mead algorithm.
+Reference: https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method
 '''
-
-x1,x2,x3,x4,x5 = sympy.symbols('x1 x2 x3 x4 x5')
-print('Input your expression:')
-f_str = input()
-str_arg = 'x0'
-itr = 0
-arguments = 0
-
-f_str = f_str.replace('^','**')
-
-while itr < 5:
-   old_str = str(itr)
-   new_str = str(itr+1)
-   str_arg = str_arg.replace(old_str,new_str)
-   print('argument to check',str_arg)
-   if str_arg in f_str:
-       arguments = arguments + 1
-       print('exists')
-   else:
-       print("doesn't exists")
-       break
-
-   itr = itr + 1
-
-def f(x):
+def f(f_str,x):
+    x1,x2,x3,x4,x5 = sympy.symbols('x1 x2 x3 x4 x5')
     l_x = len(x)
     if l_x  == 2:
         x1 = x[0]
@@ -53,14 +31,10 @@ def f(x):
         x4 = x[3]
         x5 = x[4]
     return eval(f_str)
-    #return math.pow((x[0]+2*x[1]-7),2) + math.pow((2*x[0]+x[1]-5),2)
-
-    #return math.sin(x[0]) * math.cos(x[1]) * (1. / (abs(x[2]) + 1))
-    
 
 
-def nelder_mead(f, x_start,
-                step=0.1, no_improve_thr=10e-6,
+def nelder_mead(f_str,f, x_start,
+                step=0.1, no_improve_thr=10e-3,
                 no_improv_break=10, max_iter=0,
                 alpha=1., gamma=2., rho=-0.5, sigma=0.5):
     '''
@@ -75,19 +49,20 @@ def nelder_mead(f, x_start,
         @alpha, gamma, rho, sigma (floats): parameters of the algorithm
             (see Wikipedia page for reference)
         return: tuple (best parameter array, best score)
-    '''
+        '''
 
     # init
     dim = len(x_start)
-    prev_best = f(x_start)
+    prev_best = f(f_str,x_start)
     no_improv = 0
     res = [[x_start, prev_best]]
+    bestlist = []
 
     for i in range(dim):
         x = copy.copy(x_start) #tworzenie punktów początkowych 1. kopia startowego
         x[i] = x[i] + step # 2. stworzenie nowego punktu po przesunieciu kopii w jednym wymiarze o step
-        score = f(x) #wyliczenie wartosci funkcji w nowym punkcie
-        res.append([x, score]) #dodanie nowego zestawu argumentow i wartosci funkcji do 
+        score = f(f_str,x) #wyliczenie wartosci funkcji w nowym punkcie
+        res.append([x, score]) #dodanie nowego zestawu argumentow i wartosci funkcji do listy
 
     # simplex iter
     iters = 0
@@ -96,10 +71,13 @@ def nelder_mead(f, x_start,
         # order
         res.sort(key=lambda x: x[1])
         best = res[0][1]
-
+        bestlist.append(res[0][1]) 
         # break after max_iter
         if max_iter and iters >= max_iter:
-            return res[0]
+            print('best val',res[0])
+            return bestlist
+            #return res[0]
+
         iters += 1
 
         # break after no_improv_break iterations with no improvement
@@ -112,7 +90,8 @@ def nelder_mead(f, x_start,
             no_improv += 1
 
         if no_improv >= no_improv_break:
-            return res[0]
+            print('best val',res[0])
+            return bestlist
 
         # centroid
         x0 = [0.] * (dim)
@@ -122,7 +101,7 @@ def nelder_mead(f, x_start,
 
         # reflection
         xr = x0 + alpha*(x0 - res[-1][0])
-        rscore = f(xr)
+        rscore = f(f_str,xr)
         if res[0][1] <= rscore < res[-2][1]:
             del res[-1]
             res.append([xr, rscore])
@@ -131,7 +110,7 @@ def nelder_mead(f, x_start,
         # expansion
         if rscore < res[0][1]:
             xe = x0 + gamma*(x0 - res[-1][0])
-            escore = f(xe)
+            escore = f(f_str,xe)
             if escore < rscore:
                 del res[-1]
                 res.append([xe, escore])
@@ -143,7 +122,7 @@ def nelder_mead(f, x_start,
 
         # contraction
         xc = x0 + rho*(x0 - res[-1][0])
-        cscore = f(xc)
+        cscore = f(f_str,xc)
         if cscore < res[-1][1]:
             del res[-1]
             res.append([xc, cscore])
@@ -154,24 +133,59 @@ def nelder_mead(f, x_start,
         nres = []
         for tup in res:
             redx = x1 + sigma*(tup[0] - x1)
-            score = f(redx)
+            score = f(f_str,redx)
             nres.append([redx, score])
         res = nres
 
 
-start = np.empty(arguments)
-itr = 0
-print('Function has',arguments,'arguments')
-print('Input a b for random number generation:')
+def algorytm(f_str,start):
 
-while itr < arguments:
-    print('x',itr)
-    print('a')
-    rnd_a = int(input())
-    print('b')
-    rnd_b = int(input())
-    start[itr] = random.uniform(-rnd_a, rnd_b)
-    itr = itr +1
+    result = nelder_mead(f_str, f, start)
+    #print(result)
+    print(*result, sep='\n')
 
-result = nelder_mead(f, start)
-print(result)
+def start_rand(arguments): #przyjmuje ilosc argumentów i tworzy wektor startowy
+    while itr < arguments:
+        print('x',itr+1)
+        print('a')
+        rnd_a = int(input()) #zakresy do random.uniform musi zassysać skądś po wciśnięciu przycisku na GUI
+        print('b')
+        rnd_b = int(input())
+        start[itr] = random.uniform(-rnd_a, rnd_b) #nie wiem co z tym whilem calym
+        itr = itr +1
+    return start
+
+def start_eval(f_str):
+
+    #print('Input your expression:')
+    #f_str = input()
+    str_arg = 'x0'
+    itr = 0
+    arguments = 0
+
+    f_str = f_str.replace('^','**')
+    f_str = f_str.replace('pi','math.pi')
+    f_str = f_str.replace('sin','math.sin')
+
+    while itr < 5:
+        old_str = str(itr)
+        new_str = str(itr+1)
+        str_arg = str_arg.replace(old_str,new_str)
+        print('argument to check',str_arg)
+        if str_arg in f_str:
+            arguments = arguments + 1
+            print('exists')
+        else:
+            print("doesn't exists")
+            break
+        itr = itr + 1
+
+    start = np.empty(arguments)
+    itr = 0
+    print('Function has',arguments,'arguments')
+    print('Input a b for random number generation:')
+
+    start = start_rand(arguments)
+
+    algorytm(f_str,start) #wywolanie czesci z algorytmem - podanie stringa funkcji i argumentow startowych
+ 
